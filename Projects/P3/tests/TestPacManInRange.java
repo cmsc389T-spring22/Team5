133137969e2403a6e66db1a8aa9c33fc6727dcb7
Jue12138 <jnie01@terpmail.umd.edu>
2022-03-04 19:44:09 -0500

    add TestPacManInRange.java

--- Projects/P3/tests/TestPacManInRange.java
+++ Projects/P3/tests/TestPacManInRange.java
@@ -5,6 +5,8 @@ import java.io.*;
 public class TestPacManInRange extends TestCase {
 
 	public void testPacManInRange() throws FileNotFoundException{
-		return null;
+		Ghost ghost = frame.addGhost(new Location(4, 7), "Blinky", Color.red);
+
+		assert(ghost.get_valid_moves() == "{(2,3), (3,2)}");
 	}
 }
