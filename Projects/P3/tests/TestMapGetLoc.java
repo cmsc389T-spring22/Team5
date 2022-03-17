d1b506008b98792911e7b8d1347e576b22f27f7b
dbordone <dbordone@umd.edu>
2022-03-17 19:50:35 -0400

    Adding test

--- Projects/P3/tests/TestMapGetLoc.java
+++ Projects/P3/tests/TestMapGetLoc.java
@@ -1,11 +1,21 @@
 import junit.framework.*;
+import java.util.HashSet;
 import java.awt.Color;
 import java.io.*;
 
 
-public class TestMapGetLoc {
+public class TestMapGetLoc extends TestCase{
 	
 	public void testMapGetLoc() {
-		return null;
+		try{
+			NoFrame frame = new NoFrame();
+			Ghost inky = frame.addGhost(new Location(2,2), "Inky", Color.red);
+			HashSet<Map.Type> set = frame.getMap().getLoc(new Location(2,2));
+			assertTrue(set.contains(Map.Type.COOKIE));
+		} catch (FileNotFoundException e){
+			System.out.println(e);
+			return;
+		}
+
 	}
 }
