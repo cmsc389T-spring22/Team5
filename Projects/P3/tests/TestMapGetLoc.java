13f20f28b48c6874300b40ef74a61d4ec3783ecd
dbordone <dbordone@umd.edu>
2022-03-17 20:17:28 -0400

    fixing tests

--- Projects/P3/tests/TestMapGetLoc.java
+++ Projects/P3/tests/TestMapGetLoc.java
@@ -9,7 +9,7 @@ public class TestMapGetLoc extends TestCase{
 	public void testMapGetLoc() {
 		try{
 			NoFrame frame = new NoFrame();
-			Ghost inky = frame.addGhost(new Location(2,2), "Inky", Color.red);
+			Ghost inky = frame.addGhost(new Location(3,3), "Inky", Color.red);
 			HashSet<Map.Type> set = frame.getMap().getLoc(new Location(2,2));
 			assertTrue(set.contains(Map.Type.COOKIE));
 		} catch (FileNotFoundException e){
