f43b944c3b1b93d73a619a662f8c58918b36910c
Anchit Jain <42903737+jain1407@users.noreply.github.com>
2022-05-08 20:52:28 -0400

    Update Map.java

--- Projects/P3/src/Map.java
+++ Projects/P3/src/Map.java
@@ -97,13 +97,11 @@ public class Map{
 		//update locations, components, field, and cookies
 		//the id for a cookie at (10, 1) is tok_x10_y1
 		Location loc = locations.get(name);
-		if (!field.get(loc).contains(Map.Type.COOKIE)) {
-			String cookieKey = "tok_x" + loc.x + "_y" + (loc.y + 1);
-			cookies += 1;
-			field.get(loc).remove(Map.Type.COOKIE);
-			JComponent cookieComp = components.get(cookieKey);
-			components.remove(cookieKey);
-			return cookieComp;
+		if(getLoc(loc).contains(Map.Type.COOKIE)) {
+			String cookieID = "tok_x" + loc.x + "_y" + loc.y;
+			locations.remove(cookieID);
+			cookies--;
+			return components.remove(cookieID);
 		}
 		return null;
 	}
