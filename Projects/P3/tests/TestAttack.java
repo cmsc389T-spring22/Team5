92a7ad18d14da437ab51439151db2b89bb852b05
Anchit Jain <42903737+jain1407@users.noreply.github.com>
2022-05-08 20:36:04 -0400

    Update TestAttack.java

--- Projects/P3/tests/TestAttack.java
+++ Projects/P3/tests/TestAttack.java
@@ -8,11 +8,11 @@ public class TestAttack extends TestCase {
 		NoFrame frame = new NoFrame();
 
 		// Creating Players
-		Ghost ghost = frame.addGhost(new Location(4, 4), "ghost", Color.red);
+		Ghost ghost = frame.addGhost(new Location(4, 6), "ghost", Color.red);
 		Ghost ghost1 = frame.addGhost(new Location(4, 5), "ghost", Color.green);
-		PacMan pacman = frame.addPacMan(new Location(4, 6));
+		PacMan pacman = frame.addPacMan(new Location(4, 4));
 
-		assertFalse(ghost.attack());
+		assertTrue(ghost.attack());
 		assertTrue(ghost1.attack());
 
 	}
