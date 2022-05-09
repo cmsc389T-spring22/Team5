295dc2ac19fec927e82e670e4b312f375d0f5a5a
Anchit Jain <42903737+jain1407@users.noreply.github.com>
2022-05-08 20:14:00 -0400

    Update TestMapEatCookie.java

--- Projects/P3/tests/TestMapEatCookie.java
+++ Projects/P3/tests/TestMapEatCookie.java
@@ -5,6 +5,11 @@ import java.io.*;
 public class TestMapEatCookie {
 	
 	public void testMapEatCookie() {
-		return null;
+		NoFrame frame1 = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
+		Location loc1 = new Location(5,1);
+		
+		//Creating Players
+		PacMan pacman = frame1.addPacMan(loc1);
+		assertTrue(frame1.getMap().eatCookie("pacman") == null);
 	}
 }
