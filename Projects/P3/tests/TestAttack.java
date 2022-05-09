9000a104287e347adc5f31d4406658115ad67bbc
Anchit Jain <42903737+jain1407@users.noreply.github.com>
2022-05-08 21:10:29 -0400

    Update TestAttack.java

--- Projects/P3/tests/TestAttack.java
+++ Projects/P3/tests/TestAttack.java
@@ -3,17 +3,13 @@ import java.awt.Color;
 import java.io.*;
 
 public class TestAttack extends TestCase {
-
 	public void testAttack() throws FileNotFoundException{
-		NoFrame nf = new NoFrame();
+		NoFrame frame = new NoFrame();
 
-		PacMan pacman = nf.addPacMan(new Location(1, 1));
+		// Creating Players
+		Ghost ghost = frame.addGhost(new Location(4, 4), "ghost", Color.red);
+		PacMan pacman = frame.addPacMan(new Location(4, 6));
 
-		Ghost blinky = nf.addGhost(new Location(1, 2), "Blinky", Color.red);
-		Ghost clyde = nf.addGhost(new Location(1, 3), "Clyde", Color.orange);
-
-		assertTrue(blinky.attack());
-		assertTrue(clyde.attack());
-
+		assertFalse(ghost.attack());
 	}
 }
